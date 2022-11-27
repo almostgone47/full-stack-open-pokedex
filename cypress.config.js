a053807c1f09da1df23e414589e26f40deb63ffb
almostgone47 <almostgone47@hotmail.com>
2022-11-27 15:42:07 +1100

    fix lint config for cypres

--- cypress.config.js
+++ cypress.config.js
@@ -2,7 +2,8 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
-    setupNodeEvents(on, config) {
+    // eslint-disable-next-line no-unused-vars
+    setupNodeEvents(on, _config) {
       // implement node event listeners here
     },
   },
